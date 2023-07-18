a9fae57f5fe6300fb885111439d90d4adf4c4c6f
Parag koche <60149545+Paragkoche@users.noreply.github.com>
2023-07-18 16:09:20 +0530

    ss

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import express from "express";
-import prisma from "@prisma/client"
+import prisma from "@prisma/client";
 const app = express();
-const db = new prisma.PrismaClient()
-app.use(express.static(__dirname + "\\public"));
-app.set("views", __dirname + "\\views");
+const db = new prisma.PrismaClient();
+app.use(express.static("public"));
+app.set("views", "views");
 app.set("view engine", "pug");
 
 app.get("/:id", (req, res) => {
@@ -12,8 +12,8 @@ app.get("/:id", (req, res) => {
 app.post("/", (req, res) => {
   const data = req.body;
   const d = db.dat.create({ data });
-  res.send(d)
-})
+  res.send(d);
+});
 app.listen(8080, () => {
   db.$connect();
   console.log("server started in http://localhost:8080");
