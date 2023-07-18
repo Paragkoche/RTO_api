4e0fd7b5a8f73fde5a7ca89478037fffaae0e969
Parag koche <60149545+Paragkoche@users.noreply.github.com>
2023-07-18 12:33:57 +0530

    RTO_api

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import prisma from "@prisma/client"
+const app = express();
+const db = new prisma.PrismaClient()
+app.use(express.static(__dirname + "\\public"));
+app.set("views", __dirname + "\\views");
+app.set("view engine", "pug");
+
+app.get("/:id", (req, res) => {
+  res.render("index", db.dat.findUnique({ where: { id: req.params.id } }));
+});
+app.post("/", (req, res) => {
+  const data = req.body;
+  const d = db.dat.create({ data });
+  res.send(d)
+})
+app.listen(8080, () => {
+  db.$connect();
+  console.log("server started in http://localhost:8080");
+});
